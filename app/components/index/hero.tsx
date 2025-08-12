25f0084ab0590055374b6a91065d61d98a4ae46d
andryhao <andriikhrul888@gmail.com>
2025-08-13 01:45:01 +0200

    Fix for mobile screens

--- app/components/index/hero.tsx
+++ app/components/index/hero.tsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function Hero() {
   return (
     <section className="flex items-center justify-center h-screen bg-white">
-      <a href="/about-us" className="text-9xl text-black font-bold">
+      <a
+        href="/about-us"
+        className="sm:text-xl md:text-5xl text-center lg:text-7xl xl:text-8xl 2xl:text-9xl  text-black font-bold"
+      >
         Hello world!
       </a>
     </section>
