b9af0bdc2d03bc995b3803e7c2c43f0002687e5e
andryhao <andriikhrul888@gmail.com>
2025-08-13 02:09:54 +0200

    Love letter

--- app/components/index/hero.tsx
+++ app/components/index/hero.tsx
@@ -1,14 +1,32 @@
+"use client";
+
 import React from "react";
+import Particles from "./love";
 
 export default function Hero() {
   return (
-    <section className="flex items-center justify-center h-screen bg-white">
-      <a
-        href="/about-us"
-        className="sm:text-xl md:text-5xl text-center lg:text-7xl xl:text-8xl 2xl:text-9xl  text-black font-bold"
-      >
-        Hello world!
-      </a>
+    <section
+      style={{ width: "100%", position: "relative" }}
+      className="bg-black h-screen items-center justify-center flex flex-col"
+    >
+      <div className="absolute items-center justify-center flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-center">
+          I love you Ustina
+          <br />
+          Goodnight
+        </h1>
+        <p className="text-5xl md:text-9xl">❤️</p>
+      </div>
+      <Particles
+        particleColors={["#09de4cff", "#009dffff"]}
+        particleCount={200}
+        particleSpread={10}
+        speed={0.1}
+        particleBaseSize={100}
+        moveParticlesOnHover={true}
+        alphaParticles={false}
+        disableRotation={false}
+      />
     </section>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Hero from "./components/index/hero";
 import Footer from "./components/index/footer";
+import Particles from "./components/index/love";
 
 export default function HomePage() {
   return (
     <main>
-      <Hero />
-      <Footer />
+      <Hero />{" "}
     </main>
   );
 }
